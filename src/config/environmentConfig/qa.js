2dffd72e393e42ea0bb4f8ef6b5bd03fd38cc57c
Smriti Chawla <smriti.chawla@tothenew.com>
2017-08-25 15:55:15 +0530

    corrected the configuration for various environments

--- src/config/environmentConfig/qa.js
+++ src/config/environmentConfig/qa.js
@@ -1,29 +1,4 @@
 export default {
-    API_BASE_URL: 'https://tatasky-kong.videoready.tv/',
-    API_VERSION: 'v1/',
-    MIXPANEL_KEY: "d84a7ea43df441879a4b8b1e612a6fa3",
-    PUBNUB: {
-        PUBLISH_KEY: 'pub-c-110c6222-5b0a-400a-9fe4-0b2844a2577e',
-        SUBSCRIBE_KEY: 'sub-c-adfb69da-a705-11e6-af18-02ee2ddab7fe'
-    },
-    SSR: false,
-    CLOUDINARY_URLS: {
-        CLOUD_ACCOUNT_URL: "https://res.cloudinary.com/tatasky-qa/image/fetch/",
-        CLOUD_SUB_ACCOUNT_URL: "https://res.cloudinary.com/tatasky-qa/image/fetch/"
-    },
-    SSO_SIGNUP_URL: "https://uat.mytatasky.com/web/portal/signup",
-    FAQ_URL: {
-        HELP: "http://www.tatasky.com/wps/portal/TataSky/help",
-        TERMS_OF_USE: "http://www.tatasky.com/wps/portal/TataSky/footerpages/conditions",
-        PRIVACY_POLICY: "http://www.tatasky.com/wps/portal/TataSky/footerpages/privacypolicy",
-    },
-    FOOTER_URL: {
-        HELP_FAQ: "http://www.tatasky.com/wps/portal/TataSky/help",
-        CONTACT_US: "http://www.tatasky.com/wps/portal/TataSky/footerpages/contactus",
-        GET_TATA_SKY: "http://www.tatasky.com/wps/portal/TataSky/orderonline",
-        CORPORATE_WEBSITE: "http://www.tatasky.com/wps/portal"
-    },
-    GA: {
-        KEY: 'UA-104774297-1'
-    }
+    API_BASE_URL: '',
+
 }
--- src/config/environmentConfig/uat.js
+++ src/config/environmentConfig/uat.js
@@ -1,29 +1,4 @@
 export default {
-    API_BASE_URL: 'https://tatasky-uat-kong.videoready.tv/',
-    API_VERSION: 'v1/',
-    MIXPANEL_KEY: "d84a7ea43df441879a4b8b1e612a6fa3",
-    PUBNUB: {
-        PUBLISH_KEY: 'pub-c-b26c8706-876e-47bb-ae1c-6668f3fae138',
-        SUBSCRIBE_KEY: 'sub-c-551b36da-4f5a-11e7-bf50-02ee2ddab7fe'
-    },
-    SSR: false,
-    CLOUDINARY_URLS: {
-        CLOUD_ACCOUNT_URL: "https://res.cloudinary.com/uat-main/image/fetch/",
-        CLOUD_SUB_ACCOUNT_URL: "https://res.cloudinary.com/uat-epg/image/fetch/"
-    },
-    SSO_SIGNUP_URL: "https://uat.mytatasky.com/web/portal/signup",
-    FAQ_URL: {
-        HELP: "http://www.tatasky.com/wps/portal/TataSky/help",
-        TERMS_OF_USE: "http://www.tatasky.com/wps/portal/TataSky/footerpages/conditions",
-        PRIVACY_POLICY: "http://www.tatasky.com/wps/portal/TataSky/footerpages/privacypolicy",
-    },
-    FOOTER_URL: {
-        HELP_FAQ: "http://www.tatasky.com/wps/portal/TataSky/help",
-        CONTACT_US: "http://www.tatasky.com/wps/portal/TataSky/footerpages/contactus",
-        GET_TATA_SKY: "http://www.tatasky.com/wps/portal/TataSky/orderonline",
-        CORPORATE_WEBSITE: "http://www.tatasky.com/wps/portal"
-    },
-    GA: {
-        KEY: 'UA-87013361-3'
-    }
+    API_BASE_URL: '',
+
 }
