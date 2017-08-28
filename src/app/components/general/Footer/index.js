740c24a5bee18b1f8bb7621b408a13639ff93eaa
Smriti Chawla <smriti.chawla@tothenew.com>
2017-08-28 15:17:03 +0530

    added static content in footer

--- src/app/components/general/Footer/index.js
+++ src/app/components/general/Footer/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import StaticFooter from './StaticFooter';
+import style from './style.scss';
+
 class Footer extends Component {
     render = () =>
                 <footer className="footer">
                     Footer Section
+                    <StaticFooter/>
                 </footer>
 }
 
