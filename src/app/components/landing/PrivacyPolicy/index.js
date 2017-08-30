2a1ad36c00d07ee93dd5337187a0ed193df83144
Devendra Madheshiya <devendra.madheshiya@tothenew.com>
2017-08-30 11:07:11 +0530

    changes for helmet CSS setup

--- src/app/components/landing/PrivacyPolicy/index.js
+++ src/app/components/landing/PrivacyPolicy/index.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {Helmet} from "react-helmet";
 
 import './style.scss';
 
 class PrivacyPolicy extends Component {
     render() {
-        console.log("LOADED.........")
+        console.log("LOADED.........");
         return (
-            <div>Privacy Policy !!</div>
+            <div>
+                Privacy Policy !!
+
+                <Helmet>
+                    <style type="text/css">{`
+                        body {
+                            background: #ffffff;
+                        }
+                    `}</style>
+                </Helmet>
+
+            </div>
         );
     }
 }
